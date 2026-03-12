68623fcbbcba75e099f9462fca87d7b129d01f83
30fpzs <b2029mufdulay@pshs.edu.ph>
2026-03-12 21:02:39 +0800

    Commit for Graded Activity 2

--- q41gStrontiumDULAY.js
+++ q41gStrontiumDULAY.js
@@ -4,18 +4,25 @@ function submitRating(num)
 {
     rating = num;
 
-    let stars = document.querySelectorAll('.star');
+    let stars = document.querySelectorAll('#starRating .star');
 
     for(let i = 0; i < stars.length; i++)
         {
-            if(i < num)
+            let starNum = i + 1;
+
+            if(num >= starNum)
             {
-                stars[i].style.color = 'yellow';
+                stars[i].style.setProperty('--half', '100%');
+            }
+
+            else if(num >= starNum - 0.5)
+            {
+                stars[i].style.setProperty('--half', '50%');
             }
 
             else
             {
-                stars[i].style.color = 'gray';
+                stars[i].style.setProperty('--half', '0%');
             }
         }
 }
@@ -25,13 +32,6 @@ function addMovie()
     let title = document.getElementById("title").value;
     let year = document.getElementById("year").value;
     let genre = document.getElementById("genre").value;
-    
-    let movie = {
-        title: title,
-        year: year,
-        genre: genre,
-        rating: rating
-    };
 
     let movies = localStorage.getItem("movies");
 
@@ -44,10 +44,75 @@ function addMovie()
         movies = JSON.parse(movies);
     }
 
-    movies.push(movie);
+    let foundMovie = false;
+
+    for(let i = 0; i < movies.length; i++)
+    {
+        if(movies[i].title.toLowerCase() === title.toLowerCase())
+        {
+            movies[i].year = year;
+            movies[i].genre = genre;
+
+            movies[i].rating = (movies[i].rating + rating) / 2; //This is to average the score rating for a movie, if it is already found in the data set.
+
+            foundMovie = true; //This is to show that the movie is already found in the data set.
+            break;
+        }
+    }
+
+    if(!foundMovie)
+    {
+        let movie = {
+        title: title,
+        year: year,
+        genre: genre,
+        rating: rating
+        };
+
+        movies.push(movie);
+    }
+
     localStorage.setItem("movies", JSON.stringify(movies));
 
     displayMovies();
+
+    rating = 0;
+
+    let stars = document.querySelectorAll('#starRating .star');
+
+    for(let i = 0; i < stars.length; i++)
+    {
+        stars[i].style.setProperty('--half', '0%');
+    }
+}
+
+function createStars()
+{
+    let starRating = document.getElementById("starRating");
+
+    for(let i = 1; i <= 5; i++)
+    {
+        let star = document.createElement("span");
+        star.className = "star";
+        star.innerHTML = "★";
+
+        star.addEventListener("click", function(event)
+        {
+            let rect = star.getBoundingClientRect();
+            let clickX = event.clientX - rect.left;
+
+            if(clickX < rect.width / 2)
+            {
+                submitRating(i - 0.5);
+            }
+            else
+            {
+                submitRating(i);
+            }
+        });
+
+        starRating.appendChild(star);
+    }
 }
 
 function displayMovies()
@@ -71,17 +136,42 @@ function displayMovies()
     {
         let movie = movies[i];
         let stars = "";
-        for(let j = 0; j < movie.rating; j++)
+
+        let fullStar = Math.floor(movie.rating);
+        let halfStar = movie.rating % 1 !== 0;
+
+        for(let j = 0; j < fullStar; j++)
         {
             stars += "★";
+        }
+
+        if(halfStar)
+        {
+            stars += "⯪";
         }
 
         list.innerHTML +=
         "<div>" + 
         movie.title + " (" + movie.year + ") - " +
         movie.genre + ", Rating: " + stars +
+        " <button onclick = 'deleteMovie(" + i + ")'>Delete</button>" +
         "</div>";
     }
 }
 
+function deleteMovie(index)
+{
+    if(confirm("Are you sure you want to delete this movie?"))
+    {
+        let movies = JSON.parse(localStorage.getItem("movies"));
+        
+        movies.splice(index, 1);
+
+        localStorage.setItem("movies", JSON.stringify(movies));
+
+        displayMovies();
+    }
+}
+
+createStars();
 displayMovies();
